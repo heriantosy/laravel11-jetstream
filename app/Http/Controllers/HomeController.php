f2a79b32c9322560a072b79aace4e428927081d6
Herianto <52534945+heriantosy@users.noreply.github.com>
2024-05-03 12:49:07 -0400

    s

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+        if (Auth::user()->roles=='user')
+        {
+            return view('dashboard');
+        }
+        else
+        {
+            return view('admin.home');
+
+        }
+    }
+}
